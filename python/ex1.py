aeb9a7f5ae1fb927b7a962c5a70f14904ae25d34
Jhoctan21 <rm88206@fiap.com.br>
2021-08-18 10:28:01 -0300

    ex1

---
+++ python/ex1.py
@@ -0,0 +1,53 @@
+# print("1. Dada a lista = [12, -2, 4, 8, 29, 45, 78, 36, -17, 2, 12, 8, 3, 3, -52] faca um programa em Python que: ")
+
+lista = [12, -2, 4, 8, 29, 45, 78, 36, -17, 2, 12, 8, 3, 3, -52] 
+
+print("a) imprima o maior elemento ")
+
+maior_elemento = 0
+for num in lista:
+    if  (maior_elemento == 0 or num > maior_elemento):
+        maior_elemento = num     
+print("Maior elemento: {}".format(maior_elemento))
+print("Usando max")
+max_elemento = max(lista)
+print("Maior elmento: {}".format(max_elemento))
+print("_" * 30)
+
+print("b) imprima o menor elemento ")
+menor_elemento = 0
+for num in lista:
+    if  (menor_elemento == 0 or num < menor_elemento):
+        menor_elemento = num
+print("Menor elemento: {}".format(menor_elemento))
+print("Usando min")
+min_elemento = min(lista)
+print("Menor elemento: {}".format(min_elemento))
+print("_" * 30)
+
+print("c) imprima os numeros pares")
+lista_pares = []
+for pares in lista:
+    if pares % 2 == 0:
+        lista_pares.append(pares)
+        lista_pares.sort()
+print("Numeros pares encontrados na lista: {}".format(lista_pares))      
+print("_" * 30)
+
+print("d) imprima o numero de ocorrencias do primeiro elemento da lista ")
+ocorrencias = lista.count(12)
+print("Ocorrencias do primeiro elemento:  {} ".format(ocorrencias))
+print("_" * 30)
+
+print("e) imprima a media dos elementos ")
+media = sum(lista)/len(lista)
+print("Media da lista : ", media) 
+print("_" * 30)
+
+print("f) imprima a soma dos elementos de valor negativo ")
+soma_negativo = 0
+for n in lista:
+    if n < 0:
+        soma_negativo += n
+print("Soma dos elmentos negativos e igual a : {}".format(soma_negativo))
+print("_" * 30)
