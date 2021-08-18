81a30606b01726d9b9d26a7d5024c046d117dbb9
Jhoctan21 <rm88206@fiap.com.br>
2021-08-18 14:52:08 -0300

    ex4

---
+++ python/ex4.py
@@ -0,0 +1,14 @@
+# 4. Faça um programa utilizando um dict (dicionário) que leia dados de entrada do usuário. O
+# usuário deve entrar com os dados de uma pessoa como nome, idade e cidade onde mora.
+# Após isso, você deve imprimir os dados como o exemplo abaixo:
+# nome: João
+# idade: 20
+# cidade: São Paulo
+
+pessoas = dict()
+pessoas['nome'] = str(input('Nome: '))
+pessoas['idade'] = int(input('idade: '))
+pessoas['cidade'] = str(input('cidade: '))
+print("_" * 30)
+for k, v in pessoas.items():
+    print(f'{k}: {v}')
